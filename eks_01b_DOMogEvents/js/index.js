4e75234222976d5e2a0ebc9af6b270ed2999242c
ozlemertosun <U2407193@rts-365.dk>
2021-08-17 14:34:27 +0200

    add solution ti assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -2,5 +2,41 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
+		/* let counter = document.querySelector("#text-count");
+	textCountElement.textContent = 5;  */
+
+
+	let counter = document.querySelector("#text-count");
+	
+	let number = 5;
+
+	//der er oprettet variabler, for en min og maximum værdi;
+	let minValue = 0;
+	let maxValue = 10;
+
+	let plusButton = document.querySelector("#btn-count-increase");
+	let minusButton = document.querySelector("#btn-count-decrease");
+	counter.textContent = number;
+
+	function updateNumber(){
+		counter.textContent = number;
+	}
+
+	updateNumber();
+
+	plusButton.addEventListener("click", () => {
+		if (number < maxValue ){
+		number++;
+		updateNumber();
+		}
+	});
+
+	minusButton.addEventListener("click", () => {
+		if (number > minValue ){
+		number--;
+		updateNumber();
+		}
+	});
+
 
 }); // Afslutter: DOMContentLoaded
