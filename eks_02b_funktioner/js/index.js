2a22d1d3780ac6002232db6bc6a3d5276c28a92d
ozlemertosun <U2407193@rts-365.dk>
2021-08-19 11:26:22 +0200

    add solution to assignment 02b

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -3,4 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	const besked = "hej med dig"
+	let navn = ["Minna", "Laura", "Kenny", "Vagn", "Sheila", "Özlem", "Christian"]
+	
+	function hils(navn){
+		let randomNumber = Math.floor(Math.random()* 7); 
+		console.log(besked, navn[randomNumber]);
+		// det der er inde i parantensen er et argument
+	};
+
+	hils(navn);
+	hils(navn);
+	hils(navn);
+	//det inde i parantesen er parameter 
+
 }); // Afslutter: DOMContentLoaded
