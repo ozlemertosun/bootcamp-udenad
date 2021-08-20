6cde5c06ad95daf36d88d7cab53be084802235e3
ozlemertosun <U2407193@rts-365.dk>
2021-08-20 09:46:53 +0200

    add solution to assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -3,4 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	//lav et array, som vises i browser ved at bruge html liste
+
+	let navneListe = ["Rip", "Rap", "Rup"];
+	
+	for (i=0; i < navneListe.length; i++){
+		let element = document.createElement("li");
+		element.textContent = navneListe[i];
+		document.querySelector("#liste").appendChild(element);
+		console.log(i)
+	}
+
+	// et andet eksempel 
+
+	let navne = ["Troels", "Mads", "Louise"];
+	let ulElement = document.querySelector("#liste");
+
+	navne.forEach((navn) => {
+		ulElement.innerHTML += `<li> ${navn} </li>`;
+	});
+
 }); // Afslutter: DOMContentLoaded
